ebef3d629cf7167e1162155d81cc7f544f049f18
mconiaris <mconiaris@gmail.com>
2023-05-24 12:31:44 -0400

    Imported ship into Alien Invasion and called Blitme method

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -19,8 +20,7 @@ class AlienInvasion:
              self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        # Set Background Color
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -32,6 +32,7 @@ class AlienInvasion:
 
                 # Redaraw the screen during each pass through loop.
                 self.screen.fill(self.settings.bg_color)
+                self.ship.blitme()
 
                 # Make the most recently drawn screen visible.
                 pygame.display.flip()
